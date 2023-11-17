7a1eeaedc594459ce50a35d68a3ab48bd88700aa
Ganendro <byoma.argyoganendro@gmail.com>
2023-11-17 22:03:08 +0700

    decode and encode with 1 function

--- caesar/caesar_cipher3.py
+++ caesar/caesar_cipher3.py
@@ -1,6 +1,9 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+while direction not in ['decode','encode']:
+  print("Invalid input, try again.\n")
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
