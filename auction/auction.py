0f4e16134aa8b466677d5a0b2bb250e7d956e574
Ganendro <byoma.argyoganendro@gmail.com>
2023-11-21 21:57:10 +0700

    final auction code

--- auction/auction.py
+++ auction/auction.py
@@ -1,2 +1,28 @@
+import os
 from art import logo
 print(logo)
+
+bids={}
+bid_finished=False
+
+def highest_bidder(bidding_record):
+    highest_bid = 0
+    #bidding_record = {xxxxx:123,yyyyy:321}
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+while not bid_finished:
+    name = input("What is your name?: \n")
+    price = int(input("What is your bid?: \n"))
+    bids[name]=price
+    Continue=input("Are there any other bidder? Type 'yes' or 'no'.\n").lower()
+    if Continue == "no":
+        bid_finished = True
+        highest_bidder(bids)
+    elif Continue == "yes":
+        os.system('clear')
+
